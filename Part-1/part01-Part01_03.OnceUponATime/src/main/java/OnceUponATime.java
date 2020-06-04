207836943f10b578ca30605695423f9780ea2c98
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-04 18:53:36 +0530

    Initial Commit

---
+++ Part-1/part01-Part01_03.OnceUponATime/src/main/java/OnceUponATime.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class OnceUponATime {
+
+    public static void main(String[] args) {
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+        
+}
