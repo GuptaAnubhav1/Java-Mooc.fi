3f1b79da539993728f7f07c33dac08761fca0a76
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-04 18:57:38 +0530

    Initial Commit

---
+++ Part-1/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        String x = scanner.nextLine();
+        
+        System.out.println(x);
+        System.out.println(x);
+        System.out.println(x);
+
+    }
+}
