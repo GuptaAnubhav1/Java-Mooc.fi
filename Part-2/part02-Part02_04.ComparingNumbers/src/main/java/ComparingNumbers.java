3eabb3a7e9503770d0ad82f8c88e94b3800b4aed
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-06 17:36:22 +0530

    Initial Commit

---
+++ Part-2/part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ComparingNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.valueOf(scanner.nextLine());
+        int b = Integer.valueOf(scanner.nextLine());
+        if(a<b){
+            System.out.println(a + " is smaller than " + b);
+        } else if (a==b){
+            System.out.println(a + " is equal to " + b);
+        } else {
+            System.out.println(a + " is greater than " + b);
+        }
+
+    }
+}
