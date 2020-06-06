664879ad6b7d979839d87dd42ec9bb8ee3b0b60e
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-06 17:41:05 +0530

    Initial Commit

---
+++ Part-2/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum =0;
+        while(true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if(num==0){
+                System.out.println("Sum of the numbers: "+ sum);
+                break;
+            }else{
+                sum = sum + num;
+                continue;
+            }
+        }
+    } 
+}
