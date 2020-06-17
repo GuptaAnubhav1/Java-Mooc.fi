a80cbf09f0ce14aa16164c80a7d846f0cf6b0814
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-17 22:11:27 +0530

    Initial Commit

---
+++ Part-3/part03-Part03_05.LastInList/src/main/java/LastInList.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LastInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+        int x = list.size() - 1;
+        String S = list.get(x);
+        
+        System.out.println(S);
+    }
+}
