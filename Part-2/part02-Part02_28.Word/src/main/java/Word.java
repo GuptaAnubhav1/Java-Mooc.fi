be049610727a355be773d14aea16139fc2a92645
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-17 21:29:40 +0530

    Initial Commit

---
+++ Part-2/part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = word();
+        System.out.println(a);
+    }
+    public static String word(){
+        return "anubhav";
+    }
+
+}
