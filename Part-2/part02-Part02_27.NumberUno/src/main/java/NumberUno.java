656e7049cd13f31ebf5fc770f3734c56749dcb14
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-17 21:29:12 +0530

    Initial Commit

---
+++ Part-2/part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = numberUno();
+        System.out.print(n);
+    }
+    public static int numberUno(){
+        return 1;
+    }
+}
