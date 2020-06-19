041123a63da2f95ffb12e58cb19e9f895edf2c6e
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-19 23:07:54 +0530

    Initial Commit

---
+++ Part-3/part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOfSmallest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 9999) {
+                break;
+            }
+
+            list.add(input);
+        }
+        int smallest = list.get(0);
+
+        for(int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (smallest > number) {
+                smallest = number;
+            }
+        }
+        System.out.println("The smallest number: "+smallest);
+        System.out.println("Found at index: " + list.indexOf(smallest));
+        
+    }
+}
