6e1c4043c54d5f258d24d73dd156e3cbeb1cd56b
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-21 18:54:13 +0530

    Initial Commit

---
+++ Part-3/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PrintInRange {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
+
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+    }
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+        
+        for(int num:numbers){
+            if(num >=lowerLimit && num<=upperLimit){
+                System.out.println(num);
+            }
+        }
+        
+    }
+    
+}
