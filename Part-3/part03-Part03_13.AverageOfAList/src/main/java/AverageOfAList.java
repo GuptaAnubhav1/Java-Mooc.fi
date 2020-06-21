bf3c1a74e00201ac7804cb6de526d3e9670de91a
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-21 17:53:51 +0530

    Initial Commit

---
+++ Part-3/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+        float sum = 0;
+        for(int i:list){
+            sum +=i;                  
+        }
+        float avg = sum/list.size();
+        System.out.println("Average: "+ avg); 
+        
+        
+    }
+}
