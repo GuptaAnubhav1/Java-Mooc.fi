f4948abd3069d8772989bd2cfffc5b5c4a3decef
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-07-05 19:06:09 +0530

    Initial Commit

---
+++ Part-4/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account anubhavAccount = new Account("Anubhav's account", 100.00);
+        anubhavAccount.deposit(20);
+        System.out.println(anubhavAccount);
+    }
+}
