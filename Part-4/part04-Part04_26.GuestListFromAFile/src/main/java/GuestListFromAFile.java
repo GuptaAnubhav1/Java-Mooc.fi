fa82667fbca17f5e327ab5b72c1aed253f9c39d3
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-10-27 21:40:31 +0000

    Initial Commit

---
+++ Part-4/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -0,0 +1,40 @@
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GuestListFromAFile {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+        try(Scanner reader = new Scanner(Paths.get(file))){
+            while(reader.hasNextLine()){
+                list.add(reader.nextLine());
+            }
+        
+        }
+        System.out.println("");
+
+        System.out.println("Enter names, an empty line quits.");
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+}
